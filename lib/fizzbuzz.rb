b320b0f3d470ead6bc8f606eb009ab166307e328
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-17 23:03:40 +0100

    added comments

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,13 +2,13 @@ class Fizzbuzz
 
   def range(num1, num2)
     fizz = 3
-    buzz = 5
-    array = []
+    buzz = 5 #made fizz and buzz as variables so they can be changed easily
+    array = [] #empty array to push code into
 
 
-    for i in num1..num2 do
+    for i in num1..num2 do #range as parametres so they can easily be changed
       if i % fizz == 0 && i % buzz == 0
-        array.push("FizzBuzz")
+        array.push("FizzBuzz") #pushing the answer into the empty array at the top of the method
       elsif i % buzz == 0
         array.push("Buzz")
       elsif i % fizz == 0
@@ -17,6 +17,6 @@ class Fizzbuzz
       array.push(i)
       end
     end
-    array
+    array #calling the array at the end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,26 +2,26 @@ require_relative 'spec_helper'
 
 describe Fizzbuzz do
   game = Fizzbuzz.new #calling the class outside the tests so it doesn't have to repeat
-  test = game.range(1, 100) #calling the method. Putting in the variables 1 to 100.
+  test = game.range(1, 100) #calling the Method. Putting in the variables 1 to 100.
 
   it 'should handle a range from 0 to 100'do
 
-  expect(test[0]).to eq 1 #first line of code should be a 1.
-  expect(test.last).to eq "Buzz" #Last number should be 100, had to equal 100 because 100 is Buzz
+    expect(test[0]).to eq 1 #first line of code should be a 1.
+    expect(test.last).to eq "Buzz" #Last number should be 100, had to equal 100 because 100 is Buzz
   end
 
-it 'should assign multiples of 3 to the word Fizz' do
+  it 'should assign multiples of 3 to the word Fizz' do
 
-    expect(test[2]).to eq("Fizz") #Calling the method which is an array and number 2 is 3 as computers start from 0.
-end
+      expect(test[2]).to eq("Fizz") #Calling the method which is an array and number 2 is 3 as computers start from 0.
+  end
 
-it 'should assign multiples of 5 to the word Buzz' do
+  it 'should assign multiples of 5 to the word Buzz' do
 
-  expect(test[4]).to eq("Buzz") #Calling the method which is an array and number 4 is 5 as computers start from 0. .
-end
+    expect(test[4]).to eq("Buzz") #Calling the method which is an array and number 4 is 5 as computers start from 0. .
+  end
 
-it 'should assign multiples of 3 & 5 to the word Fizzbuzz' do
+  it 'should assign multiples of 3 & 5 to the word Fizzbuzz' do
 
-  expect(test[14]).to eq("FizzBuzz") #Calling the method which is an array and number 14 is 15 as computers start from 0. 
-end
+    expect(test[14]).to eq("FizzBuzz") #Calling the method which is an array and number 14 is 15 as computers start from 0.
+  end
 end
